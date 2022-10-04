d5a431ada1ade93c1db0674bc33fdcf87a5628a9
Sergey Mironov <mironsv@mail.ru>
2022-10-04 14:30:13 +0300

    Implement ccxt.pro and async

--- exchange_interface.py
+++ exchange_interface.py
@@ -6,9 +6,9 @@ import ccxt
 import constants
 
 
-def fetch_balance(exchange, coin_names):
-    print("---------------------------------- fetch_balance -----------------------------------")
-    balance_info = exchange.fetch_balance()
+async def fetch_balance(exchange, coin_names):
+    print(f"-{exchange.id}-------------------------------- fetch_balance -----------------------------------")
+    balance_info = await exchange.fetch_balance()
     for coin_name in coin_names:
         if coin_name in balance_info:
             print(f'free {coin_name}: {balance_info[coin_name]["free"]}')
@@ -17,16 +17,16 @@ def fetch_balance(exchange, coin_names):
             print("------------------------------")
 
 
-def print_open_orders(exchange, symbol):
-    print("---------------------------------- open_orders -----------------------------------")
-    open_orders = exchange.fetch_open_orders(symbol)
+async def print_open_orders(exchange, symbol):
+    print(f"-{exchange.id}-------------------------------- open_orders -----------------------------------")
+    open_orders = await exchange.fetch_open_orders(symbol)
     print(json.dumps(open_orders, indent=4))
 
 
-def create_new_order(exchange, symbol, type, side, amount, price):
-    print("---------------------------------- create_new_order -----------------------------------")
+async def create_new_order(exchange, symbol, type, side, amount, price):
+    print(f"-{exchange.id}-------------------------------- create_new_order -----------------------------------")
     try:
-        order = exchange.create_order(symbol, type, side, amount, price)
+        order = await exchange.create_order(symbol, type, side, amount, price)
         print(json.dumps(order, indent=4))
         return order
     except ccxt.InsufficientFunds as e:
@@ -34,42 +34,42 @@ def create_new_order(exchange, symbol, type, side, amount, price):
         return None
 
 
-def make_orders(exchange, coin_buy, coin_sell, symbol):
+async def make_orders(exchange, coin_buy, coin_sell, symbol):
     coin_list = [coin_buy, coin_sell]
     symbol_list = [symbol]
 
-    print(f"---------------------------------- fetch_tickers -----------------------------------")
-    print(json.dumps(exchange.fetch_tickers(symbols=symbol_list), indent=4))
-    fetch_balance(exchange, coin_list)
+    print(f"-{exchange.id}--------------------------------- fetch_tickers -----------------------------------")
+    print(json.dumps(await exchange.fetch_tickers(symbols=symbol_list), indent=4))
+    await fetch_balance(exchange, coin_list)
 
-    order1_for_cancel = create_new_order(exchange, symbol, "limit", "buy", amount=0.01, price=constants.ETH_PRICE_LOW)
-    order2 = create_new_order(exchange, symbol, "limit", "sell", amount=0.05, price=constants.ETH_PRICE_HIGH)
-    print_open_orders(exchange, symbol)
+    order1 = await create_new_order(exchange, symbol, "limit", "buy", amount=0.01, price=constants.ETH_PRICE_LOW)
+    order2 = await create_new_order(exchange, symbol, "limit", "sell", amount=0.05, price=constants.ETH_PRICE_HIGH)
+    await print_open_orders(exchange, symbol)
 
-    print(f"---------------------------------- cancel_order {order1_for_cancel['id']} --------------------------")
-    exchange.cancel_order(order1_for_cancel['id'], symbol)
-    print_open_orders(exchange, symbol)
+    print(f"-{exchange.id}--------------------------------- cancel_order {order1['id']} ------------------")
+    await exchange.cancel_order(order1['id'], symbol)
+    await print_open_orders(exchange, symbol)
 
-    specific_order = exchange.fetch_order(order1_for_cancel['id'], symbol)
-    print(json.dumps(specific_order, indent=4))
+    order1_fetched = await exchange.fetch_order(order1['id'], symbol)
+    print(json.dumps(order1_fetched, indent=4))
 
-    order_no_funds = create_new_order(exchange, symbol, "limit", "sell", 101.0, constants.ETH_PRICE_HIGH)
+    order_no_funds = await create_new_order(exchange, symbol, "limit", "sell", 101.0, constants.ETH_PRICE_HIGH)
 
-    fetch_balance(exchange, coin_list)
+    await fetch_balance(exchange, coin_list)
 
-    print(f"---------------------------------- cancel_all_orders -----------------------------------")
-    exchange.cancel_all_orders(symbol)
-    print_open_orders(exchange, symbol)
-    fetch_balance(exchange, coin_list)
+    print(f"-{exchange.id}------------------------------ cancel_all_orders -----------------------------------")
+    await exchange.cancel_all_orders(symbol)
+    await print_open_orders(exchange, symbol)
+    await fetch_balance(exchange, coin_list)
 
 
-def test_rate_limit(exchange, symbol):
+async def test_rate_limit(exchange, symbol):
     i = 0
     start_time = time.time()
     exchange.rateLimit = 300
-    while i < 1000:
+    while i < 10:
         try:
-            ticker = exchange.fetch_ticker(symbol)
+            ticker = await exchange.fetch_ticker(symbol)
             end_time = time.time()
             print(i, end_time - start_time)
             start_time = end_time
@@ -81,15 +81,15 @@ def test_rate_limit(exchange, symbol):
 async def test_orders(exchange):
     try:
         print(exchange.id)
-        markets = exchange.load_markets()
+        markets = await exchange.load_markets()
         coin_buy = "ETH"
         coin_sell = "USDT"
         symbol = coin_buy + "/" + coin_sell
 
         # print(json.dumps(markets[symbol], indent=4))
 
-        make_orders(exchange, coin_buy, coin_sell, symbol)
-        test_rate_limit(exchange, symbol)
+        await make_orders(exchange, coin_buy, coin_sell, symbol)
+        await test_rate_limit(exchange, symbol)
 
         return "success"
 
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import asyncio
+from asyncio import gather, run
 
 import ccxt
+import ccxt.pro as ccxtpro
 
 import exchange_interface
 
-if __name__ == '__main__':
+
+async def main():
+    print('CCXT Version:', ccxt.__version__)
     rate_limit_value = 1000
-    exchange_binance = ccxt.binance({
+    exchange_binance = ccxtpro.binance({
         'apiKey': '',
         'secret': '',
         'rateLimit': rate_limit_value  # (ms)
     })
     exchange_binance.set_sandbox_mode(True)
 
-    exchange_kucoin = ccxt.kucoin({
+    exchange_kucoin = ccxtpro.kucoin({
         'apiKey': '',
         'secret': '',
         'password': '',
@@ -21,8 +24,14 @@ if __name__ == '__main__':
     })
     exchange_kucoin.set_sandbox_mode(True)
 
-    status = asyncio.run(exchange_interface.test_orders(exchange_binance))
-    status = asyncio.run(exchange_interface.test_orders(exchange_kucoin))
+    # status = asyncio.run(exchange_interface.test_orders(exchange_binance))
+    # status = asyncio.run(exchange_interface.test_orders(exchange_kucoin))
 
-    exchange_binance.session.close()
-    exchange_kucoin.session.close()
+    loops = [exchange_interface.test_orders(exchange_binance), exchange_interface.test_orders(exchange_kucoin)]
+    await gather(*loops)
+
+    await exchange_binance.close()
+    await exchange_kucoin.close()
+
+
+run(main())
